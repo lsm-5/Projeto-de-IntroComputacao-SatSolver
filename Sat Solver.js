747a198fe356e5bdb8c0c149033c67c446fe59b3
LucasMendoncacc <32796404+LucasMendoncacc@users.noreply.github.com>
2017-12-03 00:38:13 -0200

    Update Sat Solver.js

--- Sat Solver.js
+++ Sat Solver.js
@@ -10,15 +10,19 @@ exports.solve = function(fileName) {
 function nextAssignment(currentAssignment, contador) {
     //contador é o numero maximo de possibilidades;
     let newAssignment = [];
-    let resultado;
-    resultado = metodobinario(contador-1,0);
+    let resultado = "";
+    resultado = metodobinario(contador,0);
+    if (resultado<currentAssignment.length){
+        let diferenca = currentAssignment.length - resultado.length;
+        for(i=0;i<diferenca;i++){
+            resultado = "0"+resultado;
+        }
+    }
     for (i=0; i<currentAssignment.length; i++){
         if(resultado.charAt(i)==0){
-            newAssignment[i] = false;
-        } else if (resultado.charAt(i)==1){
-            newAssignment[i] = true;
+            newAssignment[i] = 'false';
         } else {
-            newAssignment[i] = false;
+            newAssignment[i] = 'true';
         }
     }
     return newAssignment
@@ -26,8 +30,11 @@ function nextAssignment(currentAssignment, contador) {
 function metodobinario(a,r) {
     var mensagem ;
     var mensagem2;
-    if(a==1){
+    if(a==1) {
         mensagem = "1";
+        return mensagem;
+    } else if (a==0){
+        mensagem = "0";
         return mensagem;
     } else {
         var q = parseInt(a/2);
@@ -39,10 +46,7 @@ function metodobinario(a,r) {
 }
 
 function doSolve(clauses, assignment) {
-    for(i=0; i<clauses.length; i++) {
-        let linhaatual = clauses[i];
 
-    }
     let isSat = false;
     while ((!isSat) && /* must check whether this is the last assignment or not*/) {
         // does this assignment satisfy the formula? If so, make isSat true.
@@ -61,11 +65,15 @@ function readFormula(fileName) {
     var contents = fs.readFileSync('./fileName.cnf').toString();
     let text = contents.split('\n');
     let clauses = readClauses(text);
-    let variables = readVariables(text);
+    let qvariables = readVariables(clauses);
+    let variables = [];
     let specOk = checkProblemSpecification(text, clauses, variables);
     let result = { 'clauses': [], 'variables': [] };
     if (specOk == true) {
         result.clauses = clauses;
+        for(i=0;i<qvariables;i++){
+            variables = variables.concat(0)
+        }
         result.variables = variables;
     } else {
         console.log("Este arquivo apresenta erro de compatibilidade")
@@ -85,49 +93,46 @@ function readClauses(text) {
     return mensagem;
 }
 
-function readVariables(text) {
-    var a = text.length;
-    var variables = [];
-    var quantidadeI;
-    var quantidadeS = "";
-    for (i=0; i<a; i++){
-        if (text[i].charAt(0) == 'p') {
-            for(j=0;j<9999;j++){
-                if (text[i].charAt(j)>=0 || text[i].charAt(j)<=9){
-                    quantidadeS = quantidadeS.concat(text[i].charAt(j));
-                    if (text[i].charAt(j+1)==" "){
-                        quantidadeI = parseInt(quantidadeS);
-                        console.log(quantidadeI);
-                        break;
-                    }
+function readVariables(clauses) {
+    let contador = 0;
+    let comparar="";
+    for(i=0;i<clauses.length;i++){
+        for(j=0;j<clauses[i].length;j++) {
+            if((clauses[i].charAt(j)>='0'||clauses[i].charAt(j)<='9') && clauses[i].charAt(j+1)==' '){
+                comparar =  comparar+clauses[i].charAt(j);
+                comparar = parseInt(comparar);
+                if(comparar>=contador){
+                    contador = comparar;
                 }
-
+                comparar = "";
+            } else if (clauses[i].charAt(j)>='0'||clauses[i].charAt(j)<='9'){
+                comparar = comparar+clauses[i].charAt(j);
             }
         }
     }
-    for (c=0; c<quantidadeI; c++){
-        variables = variables.concat(0);
-    }
-    return variables;
+    return contador;
 }
 
 function checkProblemSpecification(text, clauses, variables) {
-    var a = text.length;
-    var quantidadeVS;
-    var quantidadeCS;
-    let j;
+    let quantidadeVS="";
+    let quantidadeCS="";
     let resultado;
-    for (i = 0; i < a; i++) {
+    let permission = true;
+    let h = 0;
+    for (i = 0; i < text.length; i++) {
         if (text[i].charAt(0) == 'p') {
-            for (j = 0; j < 9999; j++) {
-                if (text[i].charAt(j) >= 0 || text[i].charAt(j) <= 9) {
+            for (j = 0; j < text[i].length && permission==true; j++) {
+                if (text[i].charAt(j) >='0' && text[i].charAt(j) <= '9') {
                     //quantidade de variaveis em String
                     quantidadeVS = quantidadeVS+text[i].charAt(j);
-                    if (text[i].charAt(j + 1) == " ") {
-                        j = j + 1;
-                        for (j; j < text[i].length; j++) {
+                    if (text[i].charAt(j + 1) == ' ') {
+                        h = j + 2;
+                        for (h; h < text[i].length && permission==true; h++) {
                             //quantidade de clausulas em String
-                            quantidadeCS = quantidadeCS+text[i].charAt(j);
+                            quantidadeCS = quantidadeCS+text[i].charAt(h);
+                            if(h+1==text[i].length){
+                                permission = false;
+                            }
                         }
                     }
                 }
@@ -137,7 +142,7 @@ function checkProblemSpecification(text, clauses, variables) {
 
     quantidadeCS = parseInt(quantidadeCS);
     quantidadeVS = parseInt(quantidadeVS);
-    if (clauses.length == quantidadeCS && variables == quantidadeVS){
+    if (clauses.length == quantidadeCS && variables==quantidadeVS){
         resultado = true;
     } else {
         resultado = false;
@@ -145,4 +150,3 @@ function checkProblemSpecification(text, clauses, variables) {
 
     return resultado;
 }
-
